d35a9f3fe4f371488c756d6e9f092c2774370a0a
Antonio Arellano <antonio.941027@gmail.com>
2019-04-26 16:17:19 -0500

    Primer comentario en cambiosViernes26

--- fakestagram/Secrets.swift
+++ fakestagram/Secrets.swift
@@ -11,7 +11,7 @@ import Foundation
 enum Secrets {
     case host
     case token
-    //Un vil comentario otra vez en vistasXIB
+    //Un vil comentario otra vez en cambiosViernes26
     var value: String? {
         switch self {
         case .host:
