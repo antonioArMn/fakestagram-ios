0495e1ba27916e66069537a56944f212d914a52d
Antonio Arellano <antonio.941027@gmail.com>
2019-05-02 13:08:17 -0500

    Primer comentario en removeAbstractions

--- fakestagram/Secrets.swift
+++ fakestagram/Secrets.swift
@@ -11,7 +11,7 @@ import Foundation
 enum Secrets {
     case host
     case token
-    //Un vil comentario otra vez en persistanceAndCanche
+    //Un vil comentario otra vez en removeAbstractions
     var value: String? {
         switch self {
         case .host:
