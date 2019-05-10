5463b16818a2b44fc40bf0a429c11bcdee9fffde
Antonio Arellano <antonio.941027@gmail.com>
2019-05-10 12:23:12 -0500

    thumbnail xib and ProfilePostsClient

--- fakestagram/Profile/PostThumbnailCollectionViewCell.swift
+++ fakestagram/Profile/PostThumbnailCollectionViewCell.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class PostThumbnailCollectionViewCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "postThumbnailCell"
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var post: Post? {
+        didSet { updateImageView() }
+    }
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func updateImageView() {
+        guard let post = post else { return }
+        post.load { [weak self] img in
+            self?.imageView.image = img
+        }
     }
 
 }
