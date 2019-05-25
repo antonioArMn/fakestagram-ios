6b5853df35acc8fe140b01c2e379e235ede818c5
Antonio Arellano <antonio.941027@gmail.com>
2019-05-25 12:08:48 -0500

    Image picker working

--- fakestagram/Camera/CameraViewController.swift
+++ fakestagram/Camera/CameraViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class CameraViewController: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var selectImageButton: UIButton!
+    @IBOutlet weak var postButton: UIButton!
+    
+    //Properties
+    var imagePicker: ImagePicker!
+    var imageSelected = UIImage()
+    var imageSelected64: String = ""
     let client = CreatePostClient()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Title
+        navigationItem.title = "Post Image"
+        
+        //Constraints
+        imageView.backgroundColor = .gray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.40).isActive = true
+        
+        
+        selectImageButton.translatesAutoresizingMaskIntoConstraints = false
+        selectImageButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+        
+        postButton.translatesAutoresizingMaskIntoConstraints = false
+        postButton.bottomAnchor.constraint(equalTo: selectImageButton.topAnchor, constant: -30).isActive = true
+        
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
 
         // Do any additional setup after loading the view.
     }
@@ -20,8 +50,9 @@ class CameraViewController: UIViewController {
     //This action should be deleted, also button on storyboard (Post!)
     @IBAction func onTapSnap(_ sender: UIButton) {
         print("POOOOST")
-        guard let img = UIImage(named: "leopardoNieve"), let imgBase64 = img.encodedBase64() else { return }
-        let payload = CreatePostBase64(title: "Leopardo nieve - \(Date().currentTimestamp())", imageData: imgBase64)
+        //guard let img = UIImage(named: "leopardoNieve"), let imgBase64 = img.encodedBase64() else { return }
+        //let payload = CreatePostBase64(title: "Leopardo nieve - \(Date().currentTimestamp())", imageData: imgBase64)
+        let payload = CreatePostBase64(title: "From Photo Library", imageData: self.imageSelected64)
         client.create(payload: payload) { post in
             print(post)
         }
@@ -35,14 +66,19 @@ class CameraViewController: UIViewController {
         }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //New button and action Load Photo
+    @IBAction func showImagePicker(_ sender: UIButton) {
+        self.imagePicker.present(from: sender)
     }
-    */
+    
 
+}
+
+extension CameraViewController: ImagePickerDelegate {
+    
+    func didSelect(image: UIImage?) {
+        self.imageView.image = image
+        self.imageSelected = image!
+        self.imageSelected64 = self.imageSelected.encodedBase64()!
+    }
 }
